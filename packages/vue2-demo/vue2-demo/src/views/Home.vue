e41354ad687ad889159d7d4e6e3bc84b98625f1b
李惠 <lihuiyf@seeyon.com>
2021-12-17 16:24:30 +0800

    添加vue2-demo

---
+++ packages/vue2-demo/vue2-demo/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <vf-button>11111111</vf-button>
+  </div>
+</template>
+
+<script>
+import { VfButton } from 'vfox-ui'
+
+export default {
+  name: 'Home',
+  components: {
+    VfButton,
+  },
+}
+</script>
